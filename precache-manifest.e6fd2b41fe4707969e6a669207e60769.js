bd5322e8cca760f5aefa90a85fca767c054808b9
Selen Gora <goraselen@gmail.com>
2019-06-29 15:43:25 +0300

    Updates

---
+++ precache-manifest.e6fd2b41fe4707969e6a669207e60769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dc8d931f375aa5ab86c2b9cab00c4e",
+    "url": "/Calculator-React/index.html"
+  },
+  {
+    "revision": "9332b4c30fad075d9e85",
+    "url": "/Calculator-React/static/css/main.3f209e19.chunk.css"
+  },
+  {
+    "revision": "1fdb998c78d888c154de",
+    "url": "/Calculator-React/static/js/2.fdfc7398.chunk.js"
+  },
+  {
+    "revision": "9332b4c30fad075d9e85",
+    "url": "/Calculator-React/static/js/main.4042b72f.chunk.js"
+  },
+  {
+    "revision": "ebc029b79e4c61436295",
+    "url": "/Calculator-React/static/js/runtime~main.81ca5fbe.js"
+  },
+  {
+    "revision": "5dae43422a3433839a48abb5268b0ed3",
+    "url": "/Calculator-React/static/media/iphonex.5dae4342.png"
+  }
+]);
